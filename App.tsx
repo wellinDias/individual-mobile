a76fb175c2f0df584e6da4f72dd79bf3a3f6b088
Wellington <diaswellin1@gmail.com>
2024-06-19 10:52:25 -0300

    adicionando comentário sobre header

--- App.tsx
+++ App.tsx
@@ -5,6 +5,9 @@ import { Login } from "./src/Pages/Login";
 import { Routes } from "./src/Routes";
 import { Profile } from "./src/Pages/Profile";
 
+//Comentário sobre HEADER
+//Professor, usei o comando para a header sumir, mas não sumiu da Home e nem do Profile
+
 export default function App() {
   return (
     <>
